848c52f3f7f7bf381aae52061e871112e524ca34
sm1ky <145751659+sm1ky@users.noreply.github.com>
2023-11-30 18:33:10 +0200

    Update model.js
    если email не найден при изменении, он создаст его. Так будет удобнее изменять пользователям количество ip. Всегда уверен он или создаст нового в файле или обновит старого по одному запросу.

--- api/model.js
+++ api/model.js
@@ -7,7 +7,6 @@ const DBSqlite3 = require("../db/DBSqlite3");
 
 class Model {
   constructor() {
-    this.usersCsvPath = join(__dirname, "../", "users.csv");
     this.usersJsonPath = join(__dirname, "../", "users.json");
   }
 
@@ -73,25 +72,23 @@ class Model {
       {check = true};
     }
 
-    if (check === false)
+    if (check === false) {
+      file.push([data.email, data.limit]);
+
+      fs.writeFileSync(this.usersJsonPath, JSON.stringify(file));
+    } else {
+      file = file.map( el => {
+        if (el.includes(data.email)) {
+          return [data.email, data.limit]
+        } else return el
+      })
+
+      fs.writeFileSync(this.usersJsonPath, JSON.stringify(file));
+
       return response({
-        error: {
-          type: "NOT_FOUND",
-          reason: "This email does not exist",
-        },
+        status: 1,
       });
-
-    file = file.map( el => {
-      if (el.includes(data.email)) {
-        return [data.email, data.limit]
-      } else return el
-    })
-
-    fs.writeFileSync(this.usersJsonPath, JSON.stringify(file));
-
-    return response({
-      status: 1,
-    });
+    }
   }
 
   /**
